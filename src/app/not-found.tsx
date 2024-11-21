4e496aef3aa38e4db7ce49cf33f2d42e605a1591
David-Rapeckman <Rapeckmandavid@gmail.com>
2024-11-21 04:29:49 -0300

    alteracoes gerais de css, e rotas

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,12 @@
-// app/not-found.tsx
 import React from 'react';
+import Header from '../Components/Header/Header';
 
 const NotFoundPage = () => {
     return (
-        <div>
+        <div style={{ textAlign: 'center', padding: '50px', backgroundColor: '#f8d7da', color: '#721c24' }}>
             <h1>Página Não Encontrada</h1>
             <p>Desculpe, a página que você está procurando não existe.</p>
-            <a href="/">Voltar para a página inicial</a>
+            <a href="/" style={{ color: '#007bff', textDecoration: 'underline' }}>Voltar para a página inicial</a>
         </div>
     );
 };
