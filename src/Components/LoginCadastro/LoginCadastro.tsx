def1087aaaec328786c25ccb1163403e9d8acd85
David-Rapeckman <Rapeckmandavid@gmail.com>
2024-11-21 02:03:27 -0300

    login / cadastro

---
+++ src/Components/LoginCadastro/LoginCadastro.tsx
@@ -0,0 +1,34 @@
+// app/components/LoginCadastro.tsx
+import React, { useState } from 'react';
+import { logarEmpresa } from './services/apiService';
+
+const LoginCadastro = () => {
+    const [loginData, setLoginData] = useState({
+        login: '',
+        senha: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setLoginData({ ...loginData, [name]: value });
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        const response = await logarEmpresa(loginData);
+        console.log('Login realizado:', response);
+    };
+
+    return (
+        <div>
+            <h1>Login de Empresa</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="login" placeholder="Login" onChange={handleChange} required />
+                <input type="password" name="senha" placeholder="Senha" onChange={handleChange} required />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default LoginCadastro;
