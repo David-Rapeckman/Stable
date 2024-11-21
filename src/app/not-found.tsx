d61390a0aa17eafe10cc903e9d837439fa84a3b0
David-Rapeckman <Rapeckmandavid@gmail.com>
2024-11-21 02:02:35 -0300

    not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+// app/not-found.tsx
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h1>Página Não Encontrada</h1>
+            <p>Desculpe, a página que você está procurando não existe.</p>
+            <a href="/">Voltar para a página inicial</a>
+        </div>
+    );
+};
+
+export default NotFoundPage;
